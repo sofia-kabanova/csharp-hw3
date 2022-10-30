3b6e4395922509bc1e48ab745b86a826f40b9b2b
Sofia Kabanova <sofia.kabanova@gmail.com>
2022-10-30 19:19:25 +0300

    done 19

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,4 +1,4 @@
-﻿// 23. Напишите программу, которая
+// 23. Напишите программу, которая
 // принимает на вход число N
 // выдает таблицу кубов чисел от 1 до N
 // 3 -> 1, 8, 27
@@ -7,10 +7,6 @@ Console.Clear();
 Console.Write("Введите число N: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int count = 1;
-if (num <= 1) Console.Write("Диапазон для поиска кубов не задан. ");
-else
-{
-Console.WriteLine($"Число:  Куб:");
 void Cube (int n, int c)
 {
     while (c<=n)
